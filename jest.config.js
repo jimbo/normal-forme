d8bb780cc3f1c748182a8f24ce304e3fcf218f69
Jimmy Sanford <jimbo@users.noreply.github.com>
2018-12-23 18:16:36 -0700

    Configure jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const junitOptions = {
+  ancestorSeparator: ": ",
+  classNameTemplate: "{classname}",
+  outputDirectory: "dist",
+  outputName: "./junit.xml",
+  suiteName: "Jest Tests",
+  suiteNameTemplate: "{filename}",
+  titleTemplate: "{title}",
+}
+
+const config = {
+  clearMocks: true,
+  reporters: [
+    ["default", {}],
+    ["jest-junit", junitOptions]
+  ]
+}
+
+module.exports = config
